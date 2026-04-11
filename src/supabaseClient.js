78582f89b0b41fe3101f99809ebfb1463b2288fb
Mohammad Tasin <64245723+mohammadtasinbd@users.noreply.github.com>
2026-04-11 12:19:08 +0600

    Modify Supabase client settings
    Updated Supabase client configuration with new URL and anon key.

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-import.meta.env.VITE_SUPABASE_URL
-// Ei duita jinish Supabase Settings theke paben
-const supabaseUrl = 'VITE_SUPABASE_URL=https://wlnucjzvkuuyhllcyudn.supabase.co'
-const supabaseAnonKey = 'VITE_SUPABASE_ANON_KEY=sb_publishable_LrTk44uR8yul05adkOCVvA_iTukBhA0'
+
+const supabaseUrl = 'https://apnar-project-id.supabase.co'
+const supabaseAnonKey = 'sb_publishable_...' // Ekhane apnar copy kora key ta thakbe
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
