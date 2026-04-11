7eb9cd0f005ed032b017bda0ee049775b92c5fbe
Mohammad Tasin <64245723+mohammadtasinbd@users.noreply.github.com>
2026-04-11 12:21:54 +0600

    Update Supabase client URL and anon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://apnar-project-id.supabase.co'
-const supabaseAnonKey = 'sb_publishable_...' // Ekhane apnar copy kora key ta thakbe
+const supabaseUrl = 'https://wlnucjzvkuuyhllcyudn.supabase.co'
+const supabaseAnonKey = 'sb_publishable_LrTk44uR8yul05adkOCVvA_iTukBhA0' // Ekhane apnar copy kora key ta thakbe
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
